7909487db46a2bbc41eb515a37978ac9baddf948
Alsharif Nahas <eid_alshareef@hotmail.com>
2020-09-07 18:57:46 -0400

    new commit

--- src/app/module/Team.ts
+++ src/app/module/Team.ts
@@ -1,16 +1,3 @@
 export class Team {
-    competition_id: number;
-    drawn: number;
-    goal_diff: number;
-    goals_conceded: number;
-    goals_scored: number;
-    league_id: number;
-    lost: number;
-    matches: number;
     name: string;
-    points: number;
-    rank: number;
-    season_id: number;
-    team_id: number;
-    won: number;
 }
